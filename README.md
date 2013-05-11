062b7dac2e85758be63f0a64a38e074fd3ac318c
Nishibayashi Takuji <takuji31@gmail.com>
2013-05-11 19:15:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+DBIx::Class::InflateColumn::TimePiecePlus - It's new $module
+
+# SYNOPSIS
+
+    use DBIx::Class::InflateColumn::TimePiecePlus;
+
+# DESCRIPTION
+
+DBIx::Class::InflateColumn::TimePiecePlus is ...
+
+# LICENSE
+
+Copyright (C) Nishibayashi Takuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Nishibayashi Takuji <takuji31@gmail.com>
---
+++ lib/DBIx/Class/InflateColumn/TimePiecePlus.pm
@@ -0,0 +1,39 @@
+package DBIx::Class::InflateColumn::TimePiecePlus;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Class::InflateColumn::TimePiecePlus - It's new $module
+
+=head1 SYNOPSIS
+
+    use DBIx::Class::InflateColumn::TimePiecePlus;
+
+=head1 DESCRIPTION
+
+DBIx::Class::InflateColumn::TimePiecePlus is ...
+
+=head1 LICENSE
+
+Copyright (C) Nishibayashi Takuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji31@gmail.comE<gt>
+
+=cut
+
